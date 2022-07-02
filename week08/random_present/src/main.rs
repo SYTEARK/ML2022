216dc28f7cccc623ada3c6e8caa365d5a8b06fae
Axect <axect@outlook.kr>
2022-07-02 18:12:52 +0900

    Random Present

--- week08/random_present/src/main.rs
+++ week08/random_present/src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     //let a = vec!["태근", "예지", "동훈", "수진", "진흥", "상호"];
     let a = vec!["태근", "예지", "동훈", "수진", "진흥"];
     
-    let u = Uniform(0, 5);
+    let u = Uniform(0, 4);
     let idx = u.sample(1)[0];
 
     println!("당첨된 발표자는 {}님 입니다.", a[idx as usize]);
